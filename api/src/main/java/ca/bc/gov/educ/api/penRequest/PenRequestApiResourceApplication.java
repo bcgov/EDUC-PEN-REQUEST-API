34e0438c065367b88dcaecde1928978745d11657
NDENNY <nathan.denny@gov.bc.ca>
2019-11-25 15:16:28 -0800

    Fixed typos from ctrl+f

--- api/src/main/java/ca/bc/gov/educ/api/penRequest/PenRequestApiResourceApplication.java
+++ api/src/main/java/ca/bc/gov/educ/api/penRequest/PenRequestApiResourceApplication.java
@@ -3,13 +3,11 @@ package ca.bc.gov.educ.api.penRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EntityScan("ca.bc.gov.educ.api.penRequest")
 @ComponentScan("ca.bc.gov.educ.api.penRequest")
-@EnableCaching
 public class PenRequestApiResourceApplication {
 
     public static void main(String[] args) {
--- api/src/main/java/ca/bc/gov/educ/api/penRequest/props/ApplicationProperties.java
+++ api/src/main/java/ca/bc/gov/educ/api/penRequest/props/ApplicationProperties.java
@@ -1,10 +1,9 @@
 package ca.bc.gov.educ.api.penRequest.props;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ApplicationProperties {
 
-    public static final String CLIENT_ID = "SOAM-API";
+    public static final String CLIENT_ID = "PEN-REQUEST-API";
 }
