634915531f89e83acd24562d6685ae7951a731de
om <omprakashmishra3978@gmail.com>
2020-01-29 10:53:35 -0800

    spring security related changes.

--- api/src/main/java/ca/bc/gov/educ/api/penrequest/controller/PenRequestCommentsController.java
+++ api/src/main/java/ca/bc/gov/educ/api/penrequest/controller/PenRequestCommentsController.java
@@ -8,6 +8,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.val;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -15,25 +17,27 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableResourceServer
 public class PenRequestCommentsController implements PenRequestCommentEndpoint {
 
-    private final PenRequestCommentsMapper mapper = PenRequestCommentsMapper.mapper;
-    @Getter(AccessLevel.PRIVATE)
-    private final PenRequestCommentService penRequestCommentService;
+  private final PenRequestCommentsMapper mapper = PenRequestCommentsMapper.mapper;
+  @Getter(AccessLevel.PRIVATE)
+  private final PenRequestCommentService penRequestCommentService;
 
-    PenRequestCommentsController(@Autowired final PenRequestCommentService penRequestCommentService) {
-        this.penRequestCommentService = penRequestCommentService;
-    }
+  PenRequestCommentsController(@Autowired final PenRequestCommentService penRequestCommentService) {
+    this.penRequestCommentService = penRequestCommentService;
+  }
 
-    @Override
-    public List<PenRequestComments> retrieveComments(String penRequestId) {
-        val results = new ArrayList<PenRequestComments>();
-        getPenRequestCommentService().retrieveComments(UUID.fromString(penRequestId)).forEach(element -> results.add(mapper.toStructure(element)));
-        return results;
-    }
+  @Override
+  public List<PenRequestComments> retrieveComments(String penRequestId) {
+    val results = new ArrayList<PenRequestComments>();
+    getPenRequestCommentService().retrieveComments(UUID.fromString(penRequestId)).forEach(element -> results.add(mapper.toStructure(element)));
+    return results;
+  }
 
-    @Override
-    public PenRequestComments save(String penRequestId, PenRequestComments penRequestComments) {
-        return mapper.toStructure(getPenRequestCommentService().save(UUID.fromString(penRequestId), mapper.toModel(penRequestComments)));
-    }
+  @Override
+  public PenRequestComments save(String penRequestId, PenRequestComments penRequestComments) {
+    return mapper.toStructure(getPenRequestCommentService().save(UUID.fromString(penRequestId), mapper.toModel(penRequestComments)));
+  }
 }
--- api/src/main/java/ca/bc/gov/educ/api/penrequest/endpoint/PenRequestEndpoint.java
+++ api/src/main/java/ca/bc/gov/educ/api/penrequest/endpoint/PenRequestEndpoint.java
@@ -16,35 +16,33 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RequestMapping("/")
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-@EnableResourceServer
 @OpenAPIDefinition(info = @Info(title = "API for Pen Requests.", description = "This CRUD API is for Pen Requests tied to a Digital ID for a particular student in BC.", version = "1"), security = {@SecurityRequirement(name = "OAUTH2", scopes = {"READ_PEN_REQUEST", "WRITE_PEN_REQUEST"})})
 public interface PenRequestEndpoint {
 
-    @PreAuthorize("#oauth2.hasScope('READ_PEN_REQUEST')")
-    @GetMapping("/{id}")
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK"), @ApiResponse(responseCode = "404", description = "NOT FOUND")})
-    PenRequest retrievePenRequest(@PathVariable String id);
+  @PreAuthorize("#oauth2.hasScope('READ_PEN_REQUEST')")
+  @GetMapping("/{id}")
+  @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK"), @ApiResponse(responseCode = "404", description = "NOT FOUND")})
+  PenRequest retrievePenRequest(@PathVariable String id);
 
-    @PreAuthorize("#oauth2.hasScope('READ_PEN_REQUEST')")
-    @GetMapping("/")
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
-    Iterable<PenRequest> retrieveAllRequests();
+  @PreAuthorize("#oauth2.hasScope('READ_PEN_REQUEST')")
+  @GetMapping("/")
+  @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
+  Iterable<PenRequest> retrieveAllRequests();
 
-    @PreAuthorize("#oauth2.hasScope('WRITE_PEN_REQUEST')")
-    @PostMapping()
-    @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "CREATED"), @ApiResponse(responseCode = "200", description = "OK")})
-    PenRequest createPenRequest(@Validated @RequestBody PenRequest penRequest);
+  @PreAuthorize("#oauth2.hasScope('WRITE_PEN_REQUEST')")
+  @PostMapping()
+  @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "CREATED"), @ApiResponse(responseCode = "200", description = "OK")})
+  PenRequest createPenRequest(@Validated @RequestBody PenRequest penRequest);
 
-    @PreAuthorize("#oauth2.hasScope('WRITE_PEN_REQUEST')")
-    @PutMapping()
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
-    PenRequest updatePenRequest(@Validated @RequestBody PenRequest penRequest);
+  @PreAuthorize("#oauth2.hasScope('WRITE_PEN_REQUEST')")
+  @PutMapping()
+  @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
+  PenRequest updatePenRequest(@Validated @RequestBody PenRequest penRequest);
 
-    @PreAuthorize("#oauth2.hasScope('READ_PEN_REQUEST_STATUSES')")
-    @GetMapping("/statuses")
-    @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
-    List<PenRequestStatusCode> getPenRequestStatusCodes();
+  @PreAuthorize("#oauth2.hasScope('READ_PEN_REQUEST_STATUSES')")
+  @GetMapping("/statuses")
+  @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
+  List<PenRequestStatusCode> getPenRequestStatusCodes();
 
 
 }
