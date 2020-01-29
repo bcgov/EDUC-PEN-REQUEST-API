473852b5408781b33b09469129c8bcf4bb42a7a2
om <omprakashmishra3978@gmail.com>
2020-01-29 11:51:49 -0800

    spring security related changes. moved @EnableGlobalMethodSecurity annotation to the main class.

--- api/src/main/java/ca/bc/gov/educ/api/penrequest/PenRequestApiResourceApplication.java
+++ api/src/main/java/ca/bc/gov/educ/api/penrequest/PenRequestApiResourceApplication.java
@@ -3,10 +3,12 @@ package ca.bc.gov.educ.api.penrequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class PenRequestApiResourceApplication {
 
   public static void main(String[] args) {
