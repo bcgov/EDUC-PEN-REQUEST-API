bec94c5c27504f52a729c98eca7ae4e2ea117ec0
om <omprakashmishra3978@gmail.com>
2020-02-19 13:56:46 -0800

    create date and update date is ignored while sending the response back.

--- api/src/main/java/ca/bc/gov/educ/api/penrequest/mappers/PenRequestEntityMapper.java
+++ api/src/main/java/ca/bc/gov/educ/api/penrequest/mappers/PenRequestEntityMapper.java
@@ -3,15 +3,18 @@ package ca.bc.gov.educ.api.penrequest.mappers;
 import ca.bc.gov.educ.api.penrequest.model.PenRequestEntity;
 import ca.bc.gov.educ.api.penrequest.struct.PenRequest;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper(uses = UUIDMapper.class)
 @SuppressWarnings("squid:S1214")
 public interface PenRequestEntityMapper {
 
-    PenRequestEntityMapper mapper = Mappers.getMapper(PenRequestEntityMapper.class);
+  PenRequestEntityMapper mapper = Mappers.getMapper(PenRequestEntityMapper.class);
 
-    PenRequest toStructure(PenRequestEntity entity);
+  @Mapping(target = "createDate", ignore = true)
+  @Mapping(target = "updateDate", ignore = true)
+  PenRequest toStructure(PenRequestEntity entity);
 
-    PenRequestEntity toModel(PenRequest struct);
+  PenRequestEntity toModel(PenRequest struct);
 }
