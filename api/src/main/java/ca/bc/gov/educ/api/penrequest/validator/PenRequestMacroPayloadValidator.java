e4c0662459a77cc7234a51a75ae8f56fd4d28de0
om <omprakashmishra3978@gmail.com>
2020-04-06 15:16:00 -0700

    code updated for pen-383

---
+++ api/src/main/java/ca/bc/gov/educ/api/penrequest/validator/PenRequestMacroPayloadValidator.java
@@ -0,0 +1,52 @@
+package ca.bc.gov.educ.api.penrequest.validator;
+
+import ca.bc.gov.educ.api.penrequest.service.PenRequestMacroService;
+import ca.bc.gov.educ.api.penrequest.struct.PenRequest;
+import ca.bc.gov.educ.api.penrequest.struct.PenRequestMacro;
+import lombok.Getter;
+import lombok.val;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.FieldError;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static lombok.AccessLevel.PRIVATE;
+
+@Component
+public class PenRequestMacroPayloadValidator {
+  @Getter(PRIVATE)
+  private final PenRequestMacroService penRequestMacroService;
+
+  public PenRequestMacroPayloadValidator(PenRequestMacroService penRequestMacroService) {
+    this.penRequestMacroService = penRequestMacroService;
+  }
+
+  public List<FieldError> validatePayload(PenRequestMacro penRequestMacro, boolean isCreateOperation) {
+    final List<FieldError> apiValidationErrors = new ArrayList<>();
+    if (isCreateOperation && penRequestMacro.getMacroId() != null) {
+      apiValidationErrors.add(createFieldError("macroId", penRequestMacro.getMacroId(), "macroId should be null for post operation."));
+    }
+    validateMacroTypeCode(penRequestMacro.getMacroTypeCode(), apiValidationErrors);
+    return apiValidationErrors;
+  }
+
+  private void validateMacroTypeCode(String macroTypeCode, List<FieldError> apiValidationErrors) {
+    val result = getPenRequestMacroService().getMacroTypeCode(macroTypeCode);
+    if (result.isPresent()) {
+      val entity = result.get();
+      if (entity.getEffectiveDate().isAfter(LocalDate.now())) {
+        apiValidationErrors.add(createFieldError("macroTypeCode", macroTypeCode, "macroTypeCode is not yet effective."));
+      } else if (entity.getExpiryDate().isBefore(LocalDate.now())) {
+        apiValidationErrors.add(createFieldError("macroTypeCode", macroTypeCode, "macroTypeCode is expired."));
+      }
+    } else {
+      apiValidationErrors.add(createFieldError("macroTypeCode", macroTypeCode, "macroTypeCode Invalid."));
+    }
+  }
+
+  private FieldError createFieldError(String fieldName, Object rejectedValue, String message) {
+    return new FieldError("penRequest", fieldName, rejectedValue, false, null, null, message);
+  }
+}
