fa5b5830f429b2609a3b0fc391758406ca177df5
Mingwei <mingwei.y@gmail.com>
2020-07-27 11:24:17 -0700

    Fix the penRequestItClientSecret

--- tools/jenkins/get-clientsecret.sh
+++ tools/jenkins/get-clientsecret.sh
@@ -9,5 +9,5 @@ penRequestItClientID=$(getPenRequestItClientID)
 getPenRequestItClientSecret(){
     executorID= $KCADM_FILE_BIN_FOLDER/kcadm.sh get clients/$penRequestItClientID/client-secret -r $SOAM_KC_REALM_ID | grep -Po "(\{){0,1}[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}(\}){0,1}"
 }
-studentAdminClientSecret=$(getStudentAdminClientSecret)
-echo studentAdminClientSecret
+penRequestItClientSecret=$(getPenRequestItClientSecret)
+echo penRequestItClientSecret
