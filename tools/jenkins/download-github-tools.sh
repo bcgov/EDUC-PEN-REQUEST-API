1e24dc07d9fdaa91ef4fdbda33b0354ae6d9ca83
Mingwei <mingwei.y@gmail.com>
2020-07-28 10:57:19 -0700

    Fix: Consolidate shell scripts of saving secrets

--- tools/jenkins/download-github-tools.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -f /tmp/github-tools-v1.0.0-linux-x64.tar.gz ]
-then
-    echo 'Downloading and unzipping github-tools files...'
-    wget -P /tmp -nc https://github.com/bcgov/EDUC-INFRA-COMMON/raw/master/github-tools/dist/github-tools-v1.0.0/github-tools-v1.0.0-linux-x64.tar.gz
-    cd /tmp
-    tar zxvf github-tools-v1.0.0-linux-x64.tar.gz
-fi
