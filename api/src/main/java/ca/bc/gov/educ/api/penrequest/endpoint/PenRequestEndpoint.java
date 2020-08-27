739ea30110ca3066d68928c9a1c395eb32e569f9
mishraomp <omprakashmishra3978@gmail.com>
2020-08-27 12:18:11 -0700

    changes to try out sonar

--- api/src/main/java/ca/bc/gov/educ/api/penrequest/endpoint/PenRequestEndpoint.java
+++ api/src/main/java/ca/bc/gov/educ/api/penrequest/endpoint/PenRequestEndpoint.java
@@ -23,21 +23,44 @@ import java.util.concurrent.CompletableFuture;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
+/**
+ * The interface Pen request endpoint.
+ */
 @RequestMapping("/")
 @OpenAPIDefinition(info = @Info(title = "API for Pen Requests.", description = "This CRUD API is for Pen Requests tied to a Digital ID for a particular student in BC.", version = "1"), security = {@SecurityRequirement(name = "OAUTH2", scopes = {"READ_PEN_REQUEST", "WRITE_PEN_REQUEST"})})
 public interface PenRequestEndpoint {
 
+  /**
+   * Retrieve pen request pen request.
+   *
+   * @param id the id
+   * @return the pen request
+   */
   @PreAuthorize("#oauth2.hasScope('READ_PEN_REQUEST')")
   @GetMapping("/{id}")
   @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK"), @ApiResponse(responseCode = "404", description = "NOT FOUND")})
   PenRequest retrievePenRequest(@PathVariable String id);
 
+  /**
+   * Find pen requests iterable.
+   *
+   * @param digitalID the digital id
+   * @param status    the status
+   * @param pen       the pen
+   * @return the iterable
+   */
   @PreAuthorize("#oauth2.hasScope('READ_PEN_REQUEST')")
   @GetMapping
   @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
   @Tag(name = "findPenRequests", description = "This api method will accept all or individual parameters and search the DB. if any parameter is null then it will be not included in the query.")
   Iterable<PenRequest> findPenRequests(@RequestParam(name = "digitalID", required = false) String digitalID, @RequestParam(name = "status", required = false) String status, @RequestParam(name = "pen", required = false) String pen);
 
+  /**
+   * Create pen request pen request.
+   *
+   * @param penRequest the pen request
+   * @return the pen request
+   */
   @PreAuthorize("#oauth2.hasScope('WRITE_PEN_REQUEST')")
   @PostMapping
   @ApiResponses(value = {@ApiResponse(responseCode = "201", description = "CREATED"), @ApiResponse(responseCode = "200", description = "OK")})
@@ -45,27 +68,58 @@ public interface PenRequestEndpoint {
   @Transactional
   PenRequest createPenRequest(@Validated @RequestBody PenRequest penRequest);
 
+  /**
+   * Update pen request pen request.
+   *
+   * @param penRequest the pen request
+   * @return the pen request
+   */
   @PreAuthorize("#oauth2.hasScope('WRITE_PEN_REQUEST')")
   @PutMapping
   @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
   @Transactional
   PenRequest updatePenRequest(@Validated @RequestBody PenRequest penRequest);
 
+  /**
+   * Gets pen request status codes.
+   *
+   * @return the pen request status codes
+   */
   @PreAuthorize("#oauth2.hasScope('READ_PEN_REQUEST_STATUSES')")
   @GetMapping("/statuses")
   @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
   List<PenRequestStatusCode> getPenRequestStatusCodes();
 
+  /**
+   * Gets gender codes.
+   *
+   * @return the gender codes
+   */
   @PreAuthorize("#oauth2.hasScope('READ_PEN_REQUEST_CODES')")
   @GetMapping("/gender-codes")
   @ApiResponses(value = {@ApiResponse(responseCode = "200", description = "OK")})
   List<GenderCode> getGenderCodes();
 
+  /**
+   * Delete by id response entity.
+   *
+   * @param id the id
+   * @return the response entity
+   */
   @DeleteMapping("/{id}")
   @PreAuthorize("#oauth2.hasScope('DELETE_PEN_REQUEST')")
   @ApiResponses(value = {@ApiResponse(responseCode = "204", description = "NO CONTENT"), @ApiResponse(responseCode = "404", description = "NOT FOUND."), @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR.")})
   ResponseEntity<Void> deleteById(@PathVariable UUID id);
 
+  /**
+   * Find all completable future.
+   *
+   * @param pageNumber             the page number
+   * @param pageSize               the page size
+   * @param sortCriteriaJson       the sort criteria json
+   * @param searchCriteriaListJson the search criteria list json
+   * @return the completable future
+   */
   @GetMapping("/paginated")
   @Async
   @PreAuthorize("#oauth2.hasScope('READ_PEN_REQUEST')")
